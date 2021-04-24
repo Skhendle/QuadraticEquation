10f9ac690bea28fbd8c70513de92ae5526254853
Skhendle <skhendle@gmail.com>
2021-04-24 15:19:32 +0200

    CORS

--- Back End/app/main.py
+++ Back End/app/main.py
@@ -1,7 +1,24 @@
 import json, logging
 from fastapi import Depends, FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "https://localhost:8000",
+    "http://localhost:8000/*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 from app.quadratic_equation import route
 app.include_router(route.router)
